14700030bf92096a9f7c7de32a0c29dcb06eebe6
hkuhn <hkuhn@umich.edu>
2013-09-18 01:35:30 -0400

    Movement Stuff Done

---
+++ Asteroids/Asteroids/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace Asteroids
+{
+    partial class GameplayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameplayForm));
+            this.spaceship = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.spaceship)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // spaceship
+            // 
+            this.spaceship.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.spaceship.Image = ((System.Drawing.Image)(resources.GetObject("spaceship.Image")));
+            this.spaceship.Location = new System.Drawing.Point(334, 700);
+            this.spaceship.Name = "spaceship";
+            this.spaceship.Size = new System.Drawing.Size(50, 50);
+            this.spaceship.TabIndex = 0;
+            this.spaceship.TabStop = false;
+            // 
+            // GameplayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(784, 762);
+            this.Controls.Add(this.spaceship);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 800);
+            this.MinimumSize = new System.Drawing.Size(800, 800);
+            this.Name = "GameplayForm";
+            this.Text = "Asteroids";
+            this.Load += new System.EventHandler(this.Asteroids_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Asteroids_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Asteroids_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.spaceship)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox spaceship;
+    }
+}
+
---
+++ Asteroids/Asteroids/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asteroids
+{
+    public partial class GameplayForm : Form
+    {
+        
+        // class vars
+        private Boolean MoveLeft, MoveRight;
+
+        public GameplayForm()
+        {
+            InitializeComponent();
+            MoveLeft = false;
+            MoveRight = false;
+        }
+
+        private void Asteroids_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Asteroids_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            //
+            // Singular Event Handlers
+            //
+            if (e.KeyCode == Keys.Left)
+            {
+                // move left
+                if (this.spaceship.Left > 0)
+                {
+                    MoveLeft = true;
+                }
+                else
+                {
+                    MoveLeft = false;
+                }
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                // move right
+                if (this.spaceship.Left < 780 - this.spaceship.Width)
+                {
+                    MoveRight = true;
+                }
+                else
+                {
+                    MoveRight = false;
+                }
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                // shoot laser
+            }
+
+            // Evaluate Movements
+            Asteroids_Evaluate();
+
+        }
+
+        private void Asteroids_Evaluate()
+        {
+            if (MoveLeft)
+            {
+                // move left
+                this.spaceship.Location = new Point(this.spaceship.Left - 5, this.spaceship.Top);
+            }
+            else if (MoveRight)
+            {
+                // move right
+                this.spaceship.Location = new Point(this.spaceship.Left + 5, this.spaceship.Top);
+            }
+        }
+
+        private void Asteroids_KeyUp(object sender, KeyEventArgs e)
+        {
+            //
+            // Singular Event Handlers
+            //
+            if (e.KeyCode == Keys.Left)
+            {
+                // move left
+                MoveLeft = false;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                // move right
+                MoveRight = false;
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {
+                // shoot laser
+            }
+
+            // Evaluate Movements
+            Asteroids_Evaluate();
+        }
+
+    }
+}
